e88c4f0be4d602b874a7f6e129468d6cf1143057
ManyaPahuja <wcscdwmanya@gmail.com>
2021-10-29 18:00:44 +0530

    frontend changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import CallToAction from './Components/CallToAction';
+import Clients from './Components/Clients';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Pricing from './Components/Pricing';
+import Service from './Components/Service';
+import Testimonial from './Components/Testimonial';
 
 function App() {
   return (
-    
+    <>
+      <Header />
+      <Service />
+      <Pricing />
+      <CallToAction />
+      <Testimonial />
+      <Clients />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
